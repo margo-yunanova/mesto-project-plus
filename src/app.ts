91d7a15bdb32957116e97feadef579b988d3962b
Margo <m.yunanova@gmail.com>
2023-11-18 22:58:55 +0300

    update mongodb uri

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/mesto");
+mongoose.connect("mongodb://127.0.0.1:27017/mesto");
 
 app.use(requestLogger);
 
