c4b03e86909a7a6852eeca969c48b69de529946a
Margo <m.yunanova@gmail.com>
2023-11-21 16:34:44 +0300

    add copying .env.deploy to server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: ".env.deploy" });
 
 const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } =
   process.env;
@@ -17,7 +17,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "pre-deploy-local": `scp .env .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       "post-deploy":
         "npm i && npm run build && pm2 startOrRestart ecosystem.config.js && pm2 save",
     },
